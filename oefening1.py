2962836f135152fb3953f484d02cb96a10e27ee2
codedvalley <codedvalley@gmail.com>
2025-10-20 14:49:19 +0200

    oefening gedaan

---
+++ oefening1.py
@@ -0,0 +1,10 @@
+# Neem de lijst namen = ['Alfred', 'Bob', 'Charlie', 'David', 'Erik'] over.
+
+# Print de eerste 3 namen
+# Vervang David en Erik door ['Daphne', 'Eva', 'Frederique'] en print de uitkomst
+
+namen = ['Alfred', 'Bob', 'Charlie', 'David', 'Erik']
+print(namen[0:3])
+
+namen[3:] = "Daphne", 'Eva', 'Frederique'
+print(namen)
---
+++ oefening2.py
@@ -0,0 +1,8 @@
+# Neem de lijst namen = ['Alfred', 'Bob', 'Charlie', 'David', 'Erik'] over.
+
+# Vervang David en Erik door ['Daphne', 'Eva', 'Frederique'] en print de uitkomst
+
+namen = ['Alfred', 'Bob', 'Charlie', 'David', 'Erik']
+
+namen [3:5] = ['Daphne' , 'Eva', 'Frederique']
+print(namen)
